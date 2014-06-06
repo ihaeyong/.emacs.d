ffda1f084b5e9ffd39e8075cad03a89c9eeef98f
HOSONO Hidetomo <h12o@blessedgeeks.org>
2014-06-07 00:22:28 +0900

    Removed a duplicated mode pragma and an unnecessary blank line.

--- user-start.d/20autorevert.el
+++ user-start.d/20autorevert.el
@@ -1,9 +1,6 @@
 ;;; -*- mode: emacs-lisp; coding: utf-8-emacs; -*-
 
-;;; -*- mode: emacs-lisp; coding: utf-8-emacs; -*-
-
 (if (locate-library "autorevert")
     (progn
       (require 'autorevert)
       (global-auto-revert-mode 1)))
-
