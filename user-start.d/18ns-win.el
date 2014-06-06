a6106d0aea1f42ccef39563b4ecd0eef6ac3582b
HOSONO Hidetomo <h12o@blessedgeeks.org>
2014-06-07 00:29:01 +0900

    * Let it do 'mac-add-key-passed-to-system only if it is found. * Added 'mac-change-language-to-us to minibuffer-setup-hook only if it is found.

--- user-start.d/18ns-win.el
+++ user-start.d/18ns-win.el
@@ -4,5 +4,7 @@
          (eq window-system 'ns)
          (display-graphic-p))
     (progn
-      (mac-add-key-passed-to-system 'shift)
-      (add-hook 'minibuffer-setup-hook 'mac-change-language-to-us)))
+      (if (fboundp 'mac-add-key-passed-to-system)
+          (mac-add-key-passed-to-system 'shift))
+      (if (fboundp 'mac-change-language-to-us)
+          (add-hook 'minibuffer-setup-hook 'mac-change-language-to-us))))
