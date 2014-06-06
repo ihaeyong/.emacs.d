67ad9b54ba6493ea6aff347ffabf63c94f71210a
HOSONO Hidetomo <h12o@blessedgeeks.org>
2014-06-07 00:38:16 +0900

    * Let it use a smaller fontset. * Added 'set-frame-font to after-init-hook.

--- user-start.d/20fontset.el
+++ user-start.d/20fontset.el
@@ -9,24 +9,29 @@
              (display-graphic-p))
         (add-to-list 'default-frame-alist '(font . "TakaoGothic-12"))
         (add-to-list 'initial-frame-alist '(font . "TakaoGothic-12"))
-        (set-frame-font "TakaoGothic-12"))
+        (add-hook 'after-init-hook
+                  (lambda () (set-frame-font "TakaoGothic-12"))))
        ((and (string-match "mingw-nt" system-configuration)
              (display-graphic-p))
         (add-to-list 'default-frame-alist '(font . "MS Gothic-12"))
         (add-to-list 'initial-frame-alist '(font . "MS Gothic-12"))
-        (set-frame-font "MS Gothic-12"))
+        (add-hook 'after-init-hook
+                  (lambda () (set-frame-font "MS Gothic-12"))))
        ((and (string-match "apple-darwin" system-configuration)
              (display-graphic-p))
         (create-fontset-from-ascii-font
-         "Menlo-14:weight=normal:slant=normal"
+         "Menlo-12:weight=normal:slant=normal"
          nil
          "menlokakugo")
         (set-fontset-font
          "fontset-menlokakugo"
          'unicode
-         (font-spec :family "Hiragino Kaku Gothic ProN" :size 16)
+         (font-spec :family "Hiragino Kaku Gothic ProN" :size 12)
          nil
          'append)
         (add-to-list 'default-frame-alist '(font . "fontset-menlokakugo"))
         (add-to-list 'initial-frame-alist '(font . "fontset-menlokakugo"))
-        (set-frame-font "fontset-menlokakugo")))))
+        (add-to-list 'face-font-rescale-alist 
+                     '(".*Hiragino Kaku Gothic ProN.*" . 1.2))
+        (add-hook 'after-init-hook
+                  (lambda () (set-frame-font "fontset-menlokakugo")))))))
